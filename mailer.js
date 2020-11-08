0406f02e81d965d2de07c5b334f27f6a77d28c1e
Aayush Mehta <aayushmehta169@gmail.com>
2020-11-08 14:03:15 +0530

    Mail content

--- mailer.js
+++ mailer.js
@@ -2,13 +2,14 @@ var transporter = require("./config/emailConfig");
 
 exports.sendMail = async (doubt) => {
     var mailid = doubt.email
+    var ques = doubt.doubt
     
         let info = await transporter.sendMail({
-            from: '"Fred Foo 👻" <foo@example.com>', // sender address
+            from: '"Rektify 👻" <foo@example.com>', // sender address
             to: mailid, // list of receivers
             subject: "Doubt Resolved", // Subject line
             text: "Hello " + mailid, // plain text body
-            html: "<b>Your doubt has been resolved</b>", // html body
+            html: `Dear user, Your question ${ques}, has been answered.`, // html body
           });
           console.log("Message sent: %s", info.messageId);
     
